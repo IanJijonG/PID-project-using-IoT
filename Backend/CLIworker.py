d3ab37287cd1e03087219756e76b2f256739a5c8
Ian17In <jijonian@gmail.com>
2026-04-20 20:35:49 -0600

    Debugging added

--- Backend/CLIworker.py
+++ Backend/CLIworker.py
@@ -3,34 +3,36 @@ import sys
 import os
 
 def run_command(cmd):
-    
     result = subprocess.run(
         cmd,
         capture_output=True,
         text=True
     )
 
+    print("---- OUTPUT ----")
+    print(result.stdout.strip())
+
     if result.returncode != 0:
         print("---- ERROR ----")
         print(result.stderr.strip())
-        print("---- OUTPUT ----")
-        print(result.stdout.strip())
         return False
 
+    print("✅ Upload OK")
     return True
 
 def compile(FQBN,PROYECTO):
     if not run_command(
-        ["arduino-cli", "compile", "--fqbn", FQBN, PROYECTO],
+        ["arduino-cli", "compile", "--fqbn", "esp32:esp32:esp32", PROYECTO],
     ):
         sys.exit(1)
 
 
 def Upload(FQBN,PROYECTO,port):
     if not run_command(
-        ["arduino-cli", "upload", "-p", port, "--fqbn", FQBN, PROYECTO],
+        ["arduino-cli", "upload", "-p", port, "--fqbn", "esp32:esp32:esp32", PROYECTO],
     ):
         sys.exit(1)
+        return True
 
 def CreateIno(code):
     os.makedirs("Project", exist_ok=True)
