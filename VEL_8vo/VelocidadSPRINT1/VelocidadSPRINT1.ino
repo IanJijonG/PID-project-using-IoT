6c75e159b8f10f6782d20d158c43852e0c38b358
Zuriel498 <153249721+Zuriel498@users.noreply.github.com>
2026-04-24 18:02:07 -0600

    Add files via upload
    PID de velocidad con serial plotter y sin casos

---
+++ VEL_8vo/VelocidadSPRINT1/VelocidadSPRINT1.ino
@@ -0,0 +1,136 @@
+//VELOCIDAD
+
+//Inicialización de Variables
+volatile int theta = 0;
+volatile int contador= 0;
+int sp = 40;
+int caso;
+#define IN1 33 //Pines del puente H
+#define IN2 32 //Pines del puente H
+float pv; 
+float pulsos = 2483.0;
+
+# define pwmPIN 25
+
+// Variables del PID
+float cv;      // Control variable (Ley de Control)
+float cv1;         // Valor anterior de cv
+float error;       // Error actual
+float error1;      // Error anterior
+float error2;      // Error dos etapas atrás
+
+// Parámetros del PID
+float Kp=6;
+float Ki=3;
+float Kd=0.2;
+float Tm = 0.01; // Tiempo de muestreo
+
+#define EncoderA 26
+#define EncoderB 27
+
+hw_timer_t *timer = NULL;
+
+volatile bool tiempo_muestreo = false;
+
+void IRAM_ATTR timerInterrupcion() {
+ tiempo_muestreo = true;
+}
+
+void IRAM_ATTR interrupcion1(){
+  if (digitalRead(EncoderA) != digitalRead(EncoderB)){
+     theta++;
+  }
+  else{
+    theta--;
+  }
+}
+
+void IRAM_ATTR interrupcion2(){
+  if (digitalRead(EncoderA) == digitalRead(EncoderB)){
+    theta++;
+  }
+  else{
+    theta--;
+  }
+}
+
+//Tiempo de muestreo
+unsigned long lastTime = 0;
+const unsigned long Ts_us = 10000; // 10 ms
+
+void setup() {
+  Serial.begin(115200);
+  //ledcAttach(pwmPIN, frecuencia, resolucion);
+
+  pinMode(EncoderA, INPUT_PULLUP);
+  pinMode(EncoderB, INPUT_PULLUP);
+
+  pinMode(IN1, OUTPUT); // Configurar IN1 como salida
+  pinMode(IN2, OUTPUT); // Configurar IN2 como salida
+  pinMode(pwmPIN, OUTPUT);
+  
+  attachInterrupt(digitalPinToInterrupt(EncoderB),interrupcion1,CHANGE); //Flanco de subida 
+  attachInterrupt(digitalPinToInterrupt(EncoderA),interrupcion2,CHANGE); //Flanco de subida 
+
+  timer = timerBegin(1000000); // 1 MHz -> 1 tick = 1 us
+  timerAttachInterrupt(timer, &timerInterrupcion);
+  timerAlarm(timer, 10000, true, 0); // 10 ms
+
+}
+
+void loop() {
+if (tiempo_muestreo){
+  tiempo_muestreo = false;
+
+  pv= theta*2.416; //Conversión de ppr a RPM
+
+  error = sp - pv;
+
+  // --- Ecuación de diferencias del PID ---
+  cv = cv1 + (Kp + Kd / Tm) * error + (-Kp + Ki * Tm - 2 * Kd / Tm) * error1 + (Kd / Tm) * error2;
+  cv1 = cv; // Guardar valor actual de cv
+  error2 = error1; // Actualizar errores
+  error1 = error;
+
+// --- Saturar la salida del PID ---
+  if (cv > 500.0) { 
+    cv = 500.0;
+  }
+  if (cv < -500.0) { 
+    cv = -500.0;
+  }
+
+  
+  if (cv >= 0) {
+    digitalWrite(IN1, HIGH);
+    digitalWrite(IN2, LOW);
+    analogWrite(pwmPIN, (abs(cv) * 255.0 / 500.0));
+    
+    } 
+    else if (cv < 0) {
+      digitalWrite(IN1, LOW);
+      digitalWrite(IN2, HIGH);
+      analogWrite(pwmPIN, (abs(cv) * 255.0 / 500.0));
+      
+    } 
+
+  Serial.print("PV:");
+  Serial.print(pv);
+  Serial.print(" ");
+  Serial.print("CV:");
+  Serial.println(cv);
+  
+  theta = 0;
+  }
+}
+
+void resetPID(){
+
+  error = 0;
+  error1 = 0;
+  error2 = 0;
+
+  cv = 0;
+  cv1 = 0;
+
+}
